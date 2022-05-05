8b1a40d62cc47d8c0f7c56ae0312b7558a7d0341
Paul St. Denis <pstdenis@widow-tll.labs.stonybrook.edu>
2022-05-05 17:20:16 -0400

    built check for lando, edited gitignore

--- db_connect.py
+++ db_connect.py
@@ -5,12 +5,13 @@ def connect():
     global currentDBIndex
     dbPostfixes=['a','b']
     dbTypes=['postgres','postgres']
-    #dbTypes=['mysql+mysqlconnector','mysql+mysqlconnector']
     with open('currentDBIndex') as x: currentDBIndex = x.read()
     currentDBIndex=1-int(currentDBIndex)
+    db='db_%s_private.csv' %dbPostfixes[currentDBIndex]
+    if os.environ.get('LANDO_INFO') is None:
+     dbConf='db_%s_private.postgres.csv' %dbPostfixes[currentDBIndex]
     x.close()
-    #with open('db_%s_private.postgres.csv' %("a"), newline='') as csvfile:
-    with open('db_%s_private.postgres.csv' %(dbPostfixes[currentDBIndex]), newline='') as csvfile:
+    with open(dbConf, newline='') as csvfile:
 	    reader = csv.DictReader(csvfile)
 	    for row in reader:	
 		    engine = create_engine('%s://%s:%s@%s/%s' %(dbTypes[currentDBIndex],row['user'],row['password'],row['host'],row['db']),pool_recycle=3600,connect_args={'connect_timeout': 600000}  , echo=False)
