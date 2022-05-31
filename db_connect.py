c8c1eba6b15c665eddfc2aae73dd52e23b3bfce3
AlmondBread <pstdenis@gmail.com>
2022-05-31 17:18:40 -0400

    sqlalchemy fix

--- db_connect.py
+++ db_connect.py
@@ -1,11 +1,12 @@
 from sqlalchemy import create_engine
 import os
 import csv
+import urllib.parse
 currentDBIndex=5
 def connect():
     global currentDBIndex
     dbPostfixes=['a','b']
-    dbTypes=['postgres','postgres']
+    dbType='postgresql+pg8000'
     with open('currentDBIndex') as x: currentDBIndex = x.read()
     currentDBIndex=1-int(currentDBIndex)
     db='db_%s_private.csv' %dbPostfixes[currentDBIndex]
@@ -15,7 +16,7 @@ def connect():
     with open(dbConf, newline='') as csvfile:
 	    reader = csv.DictReader(csvfile)
 	    for row in reader:	
-		    engine = create_engine('%s://%s:%s@%s/%s' %(dbTypes[currentDBIndex],row['user'],row['password'],row['host'],row['db']),pool_recycle=600,pool_pre_ping=True,connect_args={'connect_timeout': 600000}  , echo=False)
+		    engine = create_engine('%s://%s:%s@%s:5432/%s' %(dbType,row['user'],urllib.parse.quote_plus(row['password']),row['host'],row['db']), echo=False)
     return engine,dbPostfixes[currentDBIndex]
 def change_db():
     with open('currentDBIndex', 'w') as writer: 
